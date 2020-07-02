83f1361aa30244faef713f8f312b45127ffcd782
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2020-07-02 21:58:53 +0200

    Major rehaul after steamcmd update

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,29 @@
 # Counter-Strike Global Offensive Dedicated Server
-#
-# Version 1.0.0
 
 FROM tingvarsson/steamcmd:latest
 LABEL maintainer Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
 
-ENV CSGO_DIR $HOME/csgo
+ENV FPS_MAX=500 \
+    TICKRATE=128 \
+    PORT=27015 \
+    TV_PORT=27020 \
+    MAX_PLAYERS=14 \
+    TOKEN=0 \
+    RCON_PASSWORD="changeme" \
+    SV_PASSWORD="changeme" \
+    START_MAP="de_dust2" \
+    MAP_GROUP="mg_active" \
+    GAME_TYPE=0 \
+    GAME_MODE=1 \
+    STARTUP_OPTIONS=""
 
-ADD start.sh $HOME/start.sh
+ENV CSGO_DIR $HOME/csgo-ds
+
+COPY --chown=$USER start.sh $HOME/start.sh
+COPY --chown=$USER csgo_update.txt $HOME/csgo_update.txt
 
 WORKDIR $HOME
 
-EXPOSE 27015
-
 CMD ["./start.sh"]
+
+EXPOSE 27015/tcp 27015/udp 27020/udp
--- start.sh
+++ start.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-$STEAMCMD_DIR/steamcmd.sh +login anonymous +force_install_dir $CSGO_DIR +app_update 740 validate +quit
+STEAMCMD_ARGS=" +login anonymous "
+STEAMCMD_ARGS+="+force_install_dir ${CSGO_DIR} "
+STEAMCMD_ARGS+="+app_update 740 "
+STEAMCMD_ARGS+="+quit"
+echo "STEAMCMD_ARGS=${STEAMCMD_ARGS}"
 
-$CSGO_DIR/srcds_run -game csgo -console -usercon +game_type $GAME_TYPE \
-            +game_mode $GAME_MODE +mapgroup $MAPGROUP +map $MAP $STARTUP_OPTIONS
+"${STEAMCMD_DIR}/steamcmd.sh" ${STEAMCMD_ARGS}
 
-exit 0
+SRCDS_RUN_ARGS=" -game csgo "
+SRCDS_RUN_ARGS+="-autoupdate "
+SRCDS_RUN_ARGS+="-console "
+SRCDS_RUN_ARGS+="-net_port_try 1 "
+SRCDS_RUN_ARGS+="-steam_dir ${STEAMCMD_DIR} "
+SRCDS_RUN_ARGS+="-steamcmd_script ${HOME}/csgo_update.txt "
+SRCDS_RUN_ARGS+="-usercon "
+SRCDS_RUN_ARGS+="+fps_max ${FPS_MAX} "
+SRCDS_RUN_ARGS+="-tickrate ${TICKRATE} "
+SRCDS_RUN_ARGS+="-port ${PORT} "
+SRCDS_RUN_ARGS+="+tv_port ${TV_PORT} "
+SRCDS_RUN_ARGS+="-maxplayers_override ${MAX_PLAYERS} "
+SRCDS_RUN_ARGS+="+game_type ${GAME_TYPE} "
+SRCDS_RUN_ARGS+="+game_mode ${GAME_MODE} "
+SRCDS_RUN_ARGS+="+mapgroup ${MAP_GROUP} "
+SRCDS_RUN_ARGS+="+map ${START_MAP} "
+SRCDS_RUN_ARGS+="+sv_setsteamaccount ${TOKEN} "
+SRCDS_RUN_ARGS+="+rcon_password ${RCON_PASSWORD} "
+SRCDS_RUN_ARGS+="+sv_password ${SV_PASSWORD} "
+SRCDS_RUN_ARGS+="${STARTUP_OPTIONS}"
+echo "SRCDS_RUN_ARGS=${SRCDS_RUN_ARGS}"
+
+"${CSGO_DIR}/srcds_run" ${SRCDS_RUN_ARGS}
